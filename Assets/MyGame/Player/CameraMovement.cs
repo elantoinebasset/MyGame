4ee25c92f5712e6ad2d46062e3f6045b68ed04d9
elantoinebasset <antoine.basset17@orange.fr>
2025-08-08 23:48:13 +0200

    Fix_Camera_Freeze_whenhoveringobject

--- Assets/MyGame/Player/CameraMovement.cs
+++ Assets/MyGame/Player/CameraMovement.cs
@@ -5,20 +5,28 @@ public class CameraMovement : MonoBehaviour
     public float CameraSensitivity = 100f;
     public Transform PlayerBody;
     private float xRotation = 0f;
-        void Start()
+
+    void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Application.targetFrameRate = 200; 
+        QualitySettings.vSyncCount = 0; 
     }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * CameraSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * CameraSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * CameraSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * CameraSensitivity;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         PlayerBody.Rotate(Vector3.up * mouseX);
+
     }
+    void OnGUI()
+{
+    GUI.Label(new Rect(10, 10, 100, 20), "FPS: " + (1.0f / Time.deltaTime).ToString("F0"));
+}
 }
